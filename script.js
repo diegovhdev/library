e80c1924b4299f54e1fc35ea307cf34b13757013
diegovhdev <diego.valencia.dev@gmail.com>
2025-03-04 15:22:04 -0500

    feat(books): add container to display added books
    - add container to display added books
    - add function to render books
    - add function to change the reading status of the book
    - add styles for book cards

--- script.js
+++ script.js
@@ -9,9 +9,53 @@ function Book(author, title, pages, read) {
     this.read = read;
 }
 
+Book.prototype.toggleReadStatus = function() {
+    this.read = !this.read;
+}
+
+Book.prototype.convertToHtml = function(index) {
+    const bookCard = document.createElement("div");
+    bookCard.classList.add("book-card");
+
+    const pAuthor = document.createElement("p");
+    pAuthor.textContent = `Autor: ${this.author}`;
+
+    const h3Title = document.createElement("h3");
+    h3Title.textContent = `Titulo: ${this.title}`;
+
+    const pPages = document.createElement("p");
+    pPages.textContent = `Paginas: ${this.pages}`;
+
+    const pRead = document.createElement("p");
+    pRead.textContent = `Leido: ${this.read ? "Si" : "No"}`;
+
+    const buttonRead = document.createElement("button");
+    buttonRead.textContent = this.read ? "no leído" : "leído";
+    buttonRead.classList.add("btn-style")
+    buttonRead.dataset.index = index;
+    buttonRead.addEventListener("click", onToggleReadStatus)
+
+    bookCard.appendChild(pAuthor);
+    bookCard.appendChild(h3Title);
+    bookCard.appendChild(pPages);
+    bookCard.appendChild(pRead);
+    bookCard.appendChild(buttonRead);
+
+    return bookCard;
+}
+
+function renderLibrary(book) {
+    const libraryContainer = document.getElementById("library-container");
+    libraryContainer.innerHTML = ""
+    for (let i = 0; i < myLibrary.length; i++) {
+        libraryContainer.appendChild(myLibrary[i].convertToHtml(i));
+    }
+}
+
 function addBookToLibrary({author, title, pages, read}) {
     const book = new Book(author, title, pages, read);
     myLibrary.push(book);
+    renderLibrary();
 }
 
 const bookForm = {
@@ -70,7 +114,13 @@ function onHideBookForm(event) {
     }
 }
 
+function onToggleReadStatus(event) {
+    myLibrary[event.target.dataset.index].toggleReadStatus();
+    renderLibrary();
+}
+
 
 document.getElementById("body").addEventListener("click", onHideBookForm)
 document.getElementById("toggle-book-form-btn").addEventListener("click", onShowBookForm)
 document.getElementById("add-book-form").addEventListener("submit", getBookFormData)
+
